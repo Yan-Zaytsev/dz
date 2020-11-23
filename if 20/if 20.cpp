c0b452c248330910416cb2a9373a7040ae7cfbba
Yan-Zaytsev <74431649+Yan-Zaytsev@users.noreply.github.com>
2020-11-23 07:43:45 +0300

    Добавьте файлы проекта.

---
+++ if 20/if 20.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    cout << "Введите числа A, B, C" << endl;
+    cin >> a >> b >> c;
+    if ((b < c) && (b > 0))
+        cout << "Точка B ближе" << endl << "Расстояние между B и A = " << b - a;
+    else if ((b < c) && (b < 0))
+        cout << "Точка C ближе" << endl << "Расстояние между C и A = " << (c - a) * (-1);
+
+    if ((c < b) && (c > 0))
+        cout << "Точка C ближе" << endl << "Расстояние между C и A = " << c - a;
+    else if ((c < b) && (c < 0))
+        cout << "Точка B ближе" << endl << "Расстояние между B и A = " << (b - a) * (-1);
+
+    return 0;
+}
